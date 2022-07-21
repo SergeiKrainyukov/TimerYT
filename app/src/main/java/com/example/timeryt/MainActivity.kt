221d08809f9647ca133762969496fbab47eedac3
Sergei <sk_developer@mail.ru>
2022-07-21 12:35:03 +0300

    added purple circle

--- app/src/main/java/com/example/timeryt/MainActivity.kt
+++ app/src/main/java/com/example/timeryt/MainActivity.kt
@@ -3,20 +3,23 @@ package com.example.timeryt
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.timeryt.ui.theme.TimerYtTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Timer(modifier = Modifier.size(200.dp))
+            }
         }
     }
 }
---
+++ app/src/main/java/com/example/timeryt/Timer.kt
@@ -0,0 +1,45 @@
+package com.example.timeryt
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.timeryt.ui.theme.Purple500
+
+val TIMER_SIZE = 200.dp
+
+const val START_ANGLE = -90f
+const val SWEEP_ANGLE = -330f
+const val STROKE_WIDTH = 15f
+
+@Composable
+fun Timer(modifier: Modifier) {
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.Center
+    ) {
+        Canvas(modifier = modifier) {
+            drawArc(
+                color = Purple500,
+                startAngle = START_ANGLE,
+                sweepAngle = SWEEP_ANGLE,
+                useCenter = false,
+                size = Size(size.width, size.height),
+                style = Stroke(width = STROKE_WIDTH)
+            )
+        }
+
+    }
+}
+
+@Preview
+@Composable
+fun Preview() {
+    Timer(modifier = Modifier.size(TIMER_SIZE))
+}
