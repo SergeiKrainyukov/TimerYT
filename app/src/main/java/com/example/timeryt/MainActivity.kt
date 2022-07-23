8b23808b721723542539f8d27927fc29b9afee43
Sergei <sk_developer@mail.ru>
2022-07-23 22:57:33 +0300

    added seconds text

--- app/src/main/java/com/example/timeryt/MainActivity.kt
+++ app/src/main/java/com/example/timeryt/MainActivity.kt
@@ -6,19 +6,24 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.Surface
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.example.timeryt.ui.theme.Gray
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Box(
-                contentAlignment = Alignment.Center,
-                modifier = Modifier.fillMaxSize()
-            ) {
-                Timer(modifier = Modifier.size(200.dp))
+            Surface(color = Gray, modifier = Modifier.fillMaxSize()) {
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Timer(modifier = Modifier.size(400.dp), 10f)
+                }
             }
         }
     }
--- app/src/main/java/com/example/timeryt/Timer.kt
+++ app/src/main/java/com/example/timeryt/Timer.kt
@@ -2,6 +2,7 @@ package com.example.timeryt
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
@@ -14,28 +15,31 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
-import com.example.timeryt.ui.theme.Purple500
+import androidx.compose.ui.unit.sp
+import com.example.timeryt.ui.theme.GrayLight
+import com.example.timeryt.ui.theme.Purple
 import kotlinx.coroutines.delay
 
-private val TIMER_SIZE = 200.dp
-
-private const val START_ANGLE = -90f
-private const val STROKE_WIDTH = 15f
-private const val INITIAL_SWEEP_ANGLE = 350f
-
 @Composable
-fun Timer(modifier: Modifier) {
+fun Timer(modifier: Modifier, totalTime: Float) {
+
+    val startAngle = -90f
+    val strokeWidth = 20f
+    val initialSweepAngle = 350f
 
     var isTimerRunning by remember {
         mutableStateOf(false)
     }
 
     var sweepAngle by remember {
-        mutableStateOf(INITIAL_SWEEP_ANGLE)
+        mutableStateOf(initialSweepAngle)
+    }
+
+    var currentTime by remember {
+        mutableStateOf(totalTime)
     }
 
     Box(
@@ -45,32 +49,52 @@ fun Timer(modifier: Modifier) {
 
         LaunchedEffect(key1 = sweepAngle, key2 = isTimerRunning) {
             if (sweepAngle > 0 && isTimerRunning) {
-                delay(100L)
-                sweepAngle -= 50
+                delay(1000L)
+                currentTime -= 1L
+                sweepAngle = initialSweepAngle * currentTime / totalTime
             }
         }
 
-        Canvas(modifier = modifier) {
+        Canvas(modifier = modifier.padding(60.dp)) {
             drawArc(
-                color = Purple500,
-                startAngle = START_ANGLE,
+                color = GrayLight,
+                startAngle = 360f,
+                sweepAngle = 360f,
+                useCenter = false,
+                size = Size(size.width, size.height),
+                style = Stroke(width = strokeWidth, cap = StrokeCap.Round),
+            )
+            drawArc(
+                color = Purple,
+                startAngle = startAngle,
                 sweepAngle = sweepAngle,
                 useCenter = false,
                 size = Size(size.width, size.height),
-                style = Stroke(width = STROKE_WIDTH, cap = StrokeCap.Round)
+                style = Stroke(width = strokeWidth, cap = StrokeCap.Round)
             )
         }
+        Text(
+            text = currentTime.toInt().toString(),
+            fontSize = 44.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White
+        )
         Button(
             onClick = {
-                if (isTimerRunning) sweepAngle = INITIAL_SWEEP_ANGLE
+                if (isTimerRunning) {
+                    sweepAngle = initialSweepAngle
+                    currentTime = totalTime
+                }
                 isTimerRunning = !isTimerRunning
-            }, colors = ButtonDefaults.buttonColors(
+            },
+            colors = ButtonDefaults.buttonColors(
                 backgroundColor = if (!isTimerRunning) {
-                    Purple500
+                    Purple
                 } else {
                     Color.Red
                 }
-            )
+            ),
+            modifier = Modifier.align(Alignment.BottomCenter),
         ) {
             Text(text = stringResource(id = if (isTimerRunning) R.string.button_stop else R.string.button_start))
         }
@@ -80,5 +104,5 @@ fun Timer(modifier: Modifier) {
 @Preview
 @Composable
 fun Preview() {
-    Timer(modifier = Modifier.size(TIMER_SIZE))
+    Timer(modifier = Modifier.size(200.dp), 6f)
 }
