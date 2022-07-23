54d234b4ed08e8fae6ed4a9c3d32ae010753b2fc
Sergei <sk_developer@mail.ru>
2022-07-23 23:35:23 +0300

    added second button

--- app/src/main/java/com/example/timeryt/MainActivity.kt
+++ app/src/main/java/com/example/timeryt/MainActivity.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material.Surface
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.timeryt.ui.theme.Gray
 
--- app/src/main/java/com/example/timeryt/Timer.kt
+++ app/src/main/java/com/example/timeryt/Timer.kt
@@ -1,13 +1,15 @@
 package com.example.timeryt
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Size
@@ -30,74 +32,96 @@ fun Timer(modifier: Modifier, totalTime: Float) {
     val strokeWidth = 20f
     val initialSweepAngle = 350f
 
-    var isTimerRunning by remember {
+    var isTimerRunning by rememberSaveable {
         mutableStateOf(false)
     }
 
-    var sweepAngle by remember {
+    var sweepAngle by rememberSaveable {
         mutableStateOf(initialSweepAngle)
     }
 
-    var currentTime by remember {
+    var currentTime by rememberSaveable {
         mutableStateOf(totalTime)
     }
 
-    Box(
-        modifier = modifier,
-        contentAlignment = Alignment.Center
-    ) {
+    Column {
+        Box(
+            modifier = modifier,
+            contentAlignment = Alignment.Center
+        ) {
+            LaunchedEffect(key1 = sweepAngle, key2 = isTimerRunning) {
+                if (sweepAngle > 0 && isTimerRunning) {
+                    delay(1000L)
+                    currentTime -= 1L
+                    sweepAngle = initialSweepAngle * currentTime / totalTime
+                }
+            }
 
-        LaunchedEffect(key1 = sweepAngle, key2 = isTimerRunning) {
-            if (sweepAngle > 0 && isTimerRunning) {
-                delay(1000L)
-                currentTime -= 1L
-                sweepAngle = initialSweepAngle * currentTime / totalTime
+            Canvas(modifier = modifier.padding(60.dp)) {
+                drawArc(
+                    color = GrayLight,
+                    startAngle = 360f,
+                    sweepAngle = 360f,
+                    useCenter = false,
+                    size = Size(size.width, size.height),
+                    style = Stroke(width = strokeWidth, cap = StrokeCap.Round),
+                )
+                drawArc(
+                    color = Purple,
+                    startAngle = startAngle,
+                    sweepAngle = sweepAngle,
+                    useCenter = false,
+                    size = Size(size.width, size.height),
+                    style = Stroke(width = strokeWidth, cap = StrokeCap.Round)
+                )
+            }
+            Text(
+                text = currentTime.toInt().toString(),
+                fontSize = 44.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            )
+        }
+        Spacer(modifier = Modifier.size(100.dp))
+        Row(horizontalArrangement = Arrangement.Center, modifier = Modifier.fillMaxWidth()) {
+            Button(
+                modifier = Modifier.size(height = 60.dp, width = 160.dp),
+                onClick = {
+                    if (isTimerRunning) {
+                        sweepAngle = initialSweepAngle
+                        currentTime = totalTime
+                    }
+                    isTimerRunning = !isTimerRunning
+                },
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = if (!isTimerRunning) {
+                        Purple
+                    } else {
+                        Color.Red
+                    }
+                ),
+            ) {
+                Text(text = stringResource(id = if (isTimerRunning) R.string.button_stop else R.string.button_start))
+            }
+            Spacer(modifier = Modifier.size(30.dp))
+            Button(
+                modifier = Modifier
+                    .size(height = 60.dp, width = 150.dp)
+                    .border(
+                        width = 2.dp,
+                        color = GrayLight,
+                        shape = RoundedCornerShape(15.dp)
+                    ),
+                onClick = {},
+                colors = ButtonDefaults.buttonColors(backgroundColor = GrayLight),
+            ) {
+                Text(
+                    text = stringResource(id = if (isTimerRunning) R.string.button_pause else R.string.button_pause),
+                    color = Color.White
+                )
             }
         }
 
-        Canvas(modifier = modifier.padding(60.dp)) {
-            drawArc(
-                color = GrayLight,
-                startAngle = 360f,
-                sweepAngle = 360f,
-                useCenter = false,
-                size = Size(size.width, size.height),
-                style = Stroke(width = strokeWidth, cap = StrokeCap.Round),
-            )
-            drawArc(
-                color = Purple,
-                startAngle = startAngle,
-                sweepAngle = sweepAngle,
-                useCenter = false,
-                size = Size(size.width, size.height),
-                style = Stroke(width = strokeWidth, cap = StrokeCap.Round)
-            )
-        }
-        Text(
-            text = currentTime.toInt().toString(),
-            fontSize = 44.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color.White
-        )
-        Button(
-            onClick = {
-                if (isTimerRunning) {
-                    sweepAngle = initialSweepAngle
-                    currentTime = totalTime
-                }
-                isTimerRunning = !isTimerRunning
-            },
-            colors = ButtonDefaults.buttonColors(
-                backgroundColor = if (!isTimerRunning) {
-                    Purple
-                } else {
-                    Color.Red
-                }
-            ),
-            modifier = Modifier.align(Alignment.BottomCenter),
-        ) {
-            Text(text = stringResource(id = if (isTimerRunning) R.string.button_stop else R.string.button_start))
-        }
     }
 }
 
